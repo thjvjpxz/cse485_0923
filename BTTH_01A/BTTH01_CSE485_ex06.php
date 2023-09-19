88f4c4757bde8e1a148e9fd6d66a4949985bdf24
anya <2151160532@e.tlu.edu.vn>
2023-09-19 09:18:29 +0700

    Complete Ex6

---
+++ BTTH_01A/BTTH01_CSE485_ex06.php
@@ -0,0 +1,18 @@
+<?php
+    $keys = array(
+        "field1"=>"first",
+        "field2"=>"second",
+        "field3"=>"third"
+    );
+    $values = array(
+        "field1value"=>"dinosaur",
+        "field2value"=>"pig",
+        "field3value"=>"platypus"
+    );
+    $keysAndValues = array();
+    foreach ($keys as $key => $value) {
+        $fieldKey = $value;
+        $fieldValue = $values[$key . "value"];
+        $keysAndValues[$fieldKey] = $fieldValue;
+    }
+    print_r($keysAndValues);
