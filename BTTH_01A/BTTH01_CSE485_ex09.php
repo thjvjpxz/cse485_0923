e01f31546fc0dadc0585d9e07801585ff2cff203
anya <2151160532@e.tlu.edu.vn>
2023-09-19 09:37:26 +0700

    Update Ex9

--- BTTH_01A/BTTH01_CSE485_ex09.php
+++ BTTH_01A/BTTH01_CSE485_ex09.php
@@ -1,1 +1,10 @@
 <?php
+    $arr1 = ['1', 'B', 'C', 'E'];
+    $arr1 = array_map('strtolower', $arr1);
+
+    $arr2 = ['a', 0, null, false];
+    $arr2 = array_map('strtolower', $arr2);
+
+    print_r($arr1);
+    echo "<br>";
+    print_r($arr2);
