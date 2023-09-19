f6123b171027ab9add93976ac0ed0413a4fb9d3c
anya <2151160532@e.tlu.edu.vn>
2023-09-19 16:02:23 +0700

    Fix data

--- BTTH_01B/BTTH01_CSE485_ex02_data.sql
+++ BTTH_01B/BTTH01_CSE485_ex02_data.sql
@@ -11,25 +11,23 @@ insert into tacgia (ten_tgia) values ('Christy Hultberg');
 insert into tacgia (ten_tgia) values ('Tana Gatus');
 insert into tacgia (ten_tgia) values ('Violet Dring');
 -- ADD DATA TO theloai
-INSERT INTO theloai (ten_tloai) VALUES ('Hành động');
-INSERT INTO theloai (ten_tloai) VALUES ('Kinh dị');
-INSERT INTO theloai (ten_tloai) VALUES ('Tình cảm');
-INSERT INTO theloai (ten_tloai) VALUES ('Hài hước');
-INSERT INTO theloai (ten_tloai) VALUES ('Phiêu lưu');
-INSERT INTO theloai (ten_tloai) VALUES ('Khoa học viễn tưởng');
-INSERT INTO theloai (ten_tloai) VALUES ('Tâm lý');
-INSERT INTO theloai (ten_tloai) VALUES ('Hoạt hình');
-INSERT INTO theloai (ten_tloai) VALUES ('Lịch sử');
-INSERT INTO theloai (ten_tloai) VALUES ('Tội phạm');
+INSERT INTO theloai (ten_tloai) VALUES
+                                    ('Nhạc Trẻ'),
+                                    ('Nhạc Pop'),
+                                    ('Nhạc Rock'),
+                                    ('Nhạc Ballad'),
+                                    ('Nhạc Rap'),
+                                    ('Nhạc R&B'),
+                                    ('Nhạc Dance'),
+                                    ('Nhạc Trữ Tình'),
+                                    ('Nhạc Cách Mạng'),
+                                    ('Nhạc Thiếu Nhi');
 -- ADD DATA TO baiviet
-# Thêm 50 bài viết ngẫu nhiên
-
 # Tạo biến i để đếm số bài viết
 SET @i = 1;
 
 # Lặp qua vòng lặp 50 lần để thêm dữ liệu
 WHILE @i <= 50 DO
-
     # Tạo câu lệnh INSERT
     INSERT INTO baiviet (tieude, ten_bhat, ma_tloai, tomtat, noidung, ma_tgia) VALUES
         (CONCAT('Tiêu đề bài viết ', @i), CONCAT('Tên bài viết ', @i), FLOOR(RAND() * 10) + 1, CONCAT('Tóm tắt bài viết ', @i), CONCAT('Nội dung bài viết ', @i), FLOOR(RAND() * 10) + 1);
